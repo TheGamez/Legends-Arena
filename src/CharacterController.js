b654de2723d5a272c2ecce02c4cfb726a825ceb3
aba168 <alex.biscoveanu@yahoo.ca>
2022-03-14 22:10:49 -0700

    added character controller and basic third person camera

---
+++ src/CharacterController.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three'
+
+
+//class for character controller
+export class CharacterController {
+
+    constructor(model, camera, cameraControls) {
+        this.model = model
+        this.camera = camera
+        this.cameraControls = cameraControls
+
+        this.walkDirection = new THREE.Vector3(0, 0, 0)
+        this.rotateAngle = new THREE.Vector3(0, 1, 0)
+        this.rotateQuarternion = new THREE.Quaternion()
+        this.cameraTarget = new THREE.Vector3()
+        this.runVeclocity = 15
+
+        this.updateCameraTarget(0,0)
+
+    }
+
+    updateCameraTarget(x, z) {
+        this.camera.position.x += x
+        this.camera.position.z += z
+
+        this.cameraTarget.x = this.model.position.x
+        this.cameraTarget.z = this.model.position.z
+        this.cameraControls.target = this.cameraTarget
+    }
+
+    update(tick, key) {
+
+        this.model.quaternion.copy(this.rotateQuarternion)
+
+        this.camera.getWorldDirection(this.walkDirection)
+        this.walkDirection.y = 0
+        this.walkDirection.normalize()
+        
+        var offest = this.move(key)
+        this.walkDirection.applyAxisAngle(this.rotateAngle, offest)
+
+        const moveX = this.walkDirection.x * this.runVeclocity * tick
+        const moveZ = this.walkDirection.z * this.runVeclocity * tick
+        this.model.position.x += moveX
+        this.model.position.z += moveZ
+        this.updateCameraTarget(moveX, moveZ)
+
+    }
+
+    move (keysPressed) {
+        var directionOffset = 0
+        const W = 87
+        const A = 65
+        const S = 83
+        const D = 68
+
+        if (keysPressed[W]) {
+            
+            if (keysPressed[A]) {
+                directionOffset = Math.PI / 4
+            
+            } else if (keysPressed[D]) {
+                directionOffset = - Math.PI / 4
+            }
+        
+        } else if (keysPressed[S]) {
+            
+            if (keysPressed[A]) {
+                directionOffset = Math.PI / 4 + Math.PI / 2
+            
+            } else if (keysPressed[D]) {
+                directionOffset = -Math.PI / 4 - Math.PI / 2
+            
+            } else {
+                directionOffset = Math.PI
+            }
+        
+        } else if (keysPressed[A]) {
+            directionOffset = Math.PI / 2
+        
+        } else if (keysPressed[D]) {
+            directionOffset = - Math.PI / 2
+        }
+
+        return directionOffset
+    }
+
+}
---
+++ src/script.js
@@ -0,0 +1,218 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { OutlineEffect } from 'three/examples/jsm/effects/OutlineEffect.js';
+
+import { CharacterController } from './CharacterController'
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+// --BEIGN: Texture ------------------------------------------------------------------------------
+const loadingManager = new THREE.LoadingManager()
+const textureLoader = new THREE.TextureLoader(loadingManager)
+
+// const view = textureLoader.load('/background/bg.jpg')
+// const backgroundView = new THREE.Mesh(
+//     new THREE.PlaneGeometry(75, 62.5),
+//     new THREE.MeshStandardMaterial({
+//         map: view
+//     })
+// )
+
+// backgroundView.position.x = 10
+// backgroundView.position.y = 17
+// backgroundView.position.z = -30
+// scene.add(backgroundView)
+
+// Floor
+// const colourTexture = textureLoader.load('/dirt/color.jpg')
+// const normalTexture = textureLoader.load('/dirt/normal.jpg')
+// const floor = new THREE.Mesh(
+//     new THREE.CircleGeometry(25, 100),
+//     new THREE.MeshStandardMaterial({
+//         map: colourTexture, 
+//         normalMap: normalTexture
+//     })
+// )
+// floor.receiveShadow = false
+// floor.rotation.x = - Math.PI * 0.5
+// floor.position.y = - Math.PI * 1.85
+// scene.add(floor)
+
+// --END: Texture ------------------------------------------------------------------------------
+
+// --BEGIN: Lights ------------------------------------------------------------------------------
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = false
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+// --END: Lights ------------------------------------------------------------------------------
+
+
+// --BEGIN: Rendering ------------------------------------------------------------------------------
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.01, 10000)
+camera.position.set(48, 105, -101)
+//camera.position.set(5, 1, 10)
+camera.rotation.set(-2.3, 0.32, 2.8)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.minDistance = 80
+controls.maxDistance = 180
+controls.enableZoom = true
+controls.enableRotate = false
+controls.enablePan = true
+
+// --BEGIN: Models ------------------------------------------------------------------------------
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+var characterController;
+gltfLoader.load('/models/monster.glb', (gltf) => {
+  let monster = gltf.scene
+  monster = gltf.scene
+  monster.position.x = 5
+  monster.position.z = -10
+  scene.add(monster)
+
+  characterController = new CharacterController(monster, camera, controls)
+})
+
+gltfLoader.load('/models/robot.glb', (gltf) => {
+  let robot = gltf.scene
+  robot.position.x = -5
+  robot.position.z = 10
+  robot.position.y = -3
+  scene.add(robot)
+})
+
+gltfLoader.load('/models/robot.glb', (gltf) => {
+  let robot = gltf.scene
+  robot.position.x = -5
+  robot.position.z = 10
+  robot.position.y = -3
+  scene.add(robot)
+})
+
+gltfLoader.load('/models/demon.glb', (gltf) => {
+  let demon = gltf.scene
+  demon.position.x = -8
+  demon.position.z = 5
+  demon.position.y = -6
+  scene.add(demon)
+})
+
+gltfLoader.load('/models/map.glb', (gltf) => {
+  let map = gltf.scene
+  map.position.x = 0
+  map.position.y = -13
+  map.position.z = 0
+  scene.add(map)
+})
+
+
+// --END: Models ------------------------------------------------------------------------------
+
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+var key = {};
+var isPressed = false
+window.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  switch (e.keyCode) {
+    case 87: // w
+     key[e.keyCode] = true;
+     isPressed = true
+      break;
+    case 83: // s
+      key[e.keyCode] = true;
+      isPressed = true
+      break;
+    case 65: // a
+      key[e.keyCode] = true;
+      isPressed = true
+      break;
+    case 68: // d
+      key[e.keyCode] = true;
+      isPressed = true
+      break;
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  key[e.keyCode] = false;
+  isPressed = false
+})
+
+const clock = new THREE.Clock();
+const tick = () => {
+
+  let deltaTime = clock.getDelta();
+  if (characterController && isPressed) {
+    characterController.update(deltaTime, key);
+  }
+
+  // Update controls
+  controls.update()
+
+  // Render
+  new OutlineEffect(renderer).render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
+
+// --END: Rendering ------------------------------------------------------------------------------
