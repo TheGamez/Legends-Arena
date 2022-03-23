004ad6a03fee84f8029208d993589a8bc8b0f730
Jennifer Hung <90425932+jenniferhung0@users.noreply.github.com>
2022-03-22 17:05:25 -0700

    web change again

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/src/index.js',
+  entry: "path.resolve(__dirname, 'app/src/index.js')",
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'app/build'),
