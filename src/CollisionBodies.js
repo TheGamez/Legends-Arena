919e1701636d9173a35082c434d7133693a859d3
TheWarrior7210 <30708997+TheWarrior7210@users.noreply.github.com>
2022-03-24 17:07:44 -0700

    gravity and collision works for the characters, however movement is not working

---
+++ src/CollisionBodies.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three'
+
+export class CollisionBodies {
+
+	constructor(model) {
+		this.model = model
+	}
+
+
+}
--- src/script.js
+++ src/script.js
@@ -5,7 +5,13 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { OutlineEffect } from 'three/examples/jsm/effects/OutlineEffect.js';
 
+import * as CANNON from 'cannon-es'
+
+// CommonJS
+const { threeToCannon, ShapeType } = require('three-to-cannon');
 import { CharacterController } from './CharacterController'
+import { CollisionBodies } from './CollisionBodies'
+import { COLLISION_TYPES } from 'cannon-es'
 
 const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
@@ -93,43 +99,127 @@ gltfLoader.setDRACOLoader(dracoLoader)
 
 
 var characterController
+var characterPhysicsBody
 var mixer
+var monster
+var monsterBody
 
 gltfLoader.load('/models/monster.glb', (gltf) => {
-  let monster = gltf.scene
   monster = gltf.scene
-  monster.position.x = 5
-  monster.position.z = -10
+  monster.position.y = 30
+  monster.position.x = 10
+  monster.position.z = -30
   scene.add(monster)
+
+  // console.log("is monster scene an object 3D", monster.isObject3D)
+  // console.log("is monster scene an object 3D", monsterAsset.isObject3D)
+  // console.log('is monster body a cannon object ?', monsterBody)
+  
+  // let monsterShape = threeToCannon(monster, {type: ShapeType.BOX});
+  // console.log("monster sahpe", monsterShape)
+  // monsterBody = new CANNON.Body({mass: 5})
+  // monsterBody.addShape(monsterShape)
+  
+// BEGIN --- Physics -----> characterPhysicsBody = new CollisionBodies(monster)
+
+  const monsterShape = new CANNON.Box(new CANNON.Vec3(9.7,8.5 ,6.1))
+  monsterBody = new CANNON.Body({mass: 10})
+  monsterBody.addShape(monsterShape)
+
+  monsterBody.position.x = monster.position.x
+  monsterBody.position.y = monster.position.y
+  monsterBody.position.z = monster.position.z
+  world.addBody(monsterBody)
+
+  function animateMonster() {
+    requestAnimationFrame(animateMonster)
+  
+    //Animation Physics For Monster Model
+      monster.position.set(
+      monsterBody.position.x,
+      monsterBody.position.y,
+      monsterBody.position.z
+  )}
+
+  renderer.setAnimationLoop(animateMonster)
+
+ // END --- Physics -----
 
   mixer = new THREE.AnimationMixer(monster)
   mixer.clipAction(gltf.animations[1]).play()
 
   characterController = new CharacterController(monster, camera, controls)
+ // characterPhysicsBody = new CollisionBodies(monster)
 })
 
 gltfLoader.load('/models/robot.glb', (gltf) => {
   let robot = gltf.scene
-  robot.position.x = -5
+  // // Adding body to 3d model
+  // const robot_body = threeToCannon(robot, {type: ShapeType.BOX});
+  robot.position.x = 5
   robot.position.z = 10
-  robot.position.y = -3
+  robot.position.y = 30
   scene.add(robot)
-})
+  
+ // BEGIN --- Physics -----> characterPhysicsBody = new CollisionBodies(robot)
 
-gltfLoader.load('/models/robot.glb', (gltf) => {
-  let robot = gltf.scene
-  robot.position.x = -5
-  robot.position.z = 10
-  robot.position.y = -3
-  scene.add(robot)
+  const robotShape = new CANNON.Box(new CANNON.Vec3(10,3 ,2))
+  const robotBody = new CANNON.Body({mass: 10})
+  robotBody.addShape(robotShape)
+
+  robotBody.position.x = robot.position.x
+  robotBody.position.y = robot.position.y
+  robotBody.position.z = robot.position.z
+  world.addBody(robotBody)
+
+  function animateRobot() {
+    requestAnimationFrame(animateRobot)
+  
+    //Animation Physics For robot Model
+      robot.position.set(
+      robotBody.position.x,
+      robotBody.position.y,
+      robotBody.position.z
+  )}
+
+  renderer.setAnimationLoop(animateRobot)
+
+  // END --- Physics -----
+
 })
 
 gltfLoader.load('/models/demon.glb', (gltf) => {
   let demon = gltf.scene
-  demon.position.x = -8
+  demon.position.x = 8
   demon.position.z = 5
-  demon.position.y = -6
+  demon.position.y = 6
   scene.add(demon)
+
+  // BEGIN --- Physics -----> characterPhysicsBody = new CollisionBodies(demon)
+
+  const demonShape = new CANNON.Box(new CANNON.Vec3(9.2,8.73 ,2.9))
+  const demonBody = new CANNON.Body({mass: 10})
+  demonBody.addShape(demonShape)
+
+  demonBody.position.x = demon.position.x
+  demonBody.position.y = demon.position.y
+  demonBody.position.z = demon.position.z
+  world.addBody(demonBody)
+
+  function animateDemon() {
+    requestAnimationFrame(animateDemon)
+  
+    //Animation Physics For demon Model
+      demon.position.set(
+      demonBody.position.x,
+      demonBody.position.y,
+      demonBody.position.z
+  )}
+
+  renderer.setAnimationLoop(animateDemon)
+
+  // END --- Physics -----
+
 })
 
 gltfLoader.load('/models/map.glb', (gltf) => {
@@ -137,13 +227,55 @@ gltfLoader.load('/models/map.glb', (gltf) => {
   map.position.x = 0
   map.position.y = -13
   map.position.z = 0
-  scene.add(map)
+  //scene.add(map) 
+
+  const planeGeometry = new THREE.PlaneGeometry(100, 100)
+  const planeMesh = new THREE.Mesh(planeGeometry)
+  planeMesh.wireframe =  true
+  planeMesh.rotateX(-Math.PI / 2)
+  planeMesh.receiveShadow = true
+
+  planeMesh.position.x = -10
+  planeMesh.position.y = -13
+  planeMesh.position.z = 0
+  scene.add(planeMesh)
+
+  
+  // BEGIN --- Physics -----> characterPhysicsBody = new CollisionBodies(map)
+  
+  const planeShape = new CANNON.Plane()
+  const planeBody = new CANNON.Body({ mass: 0 })
+  planeBody.addShape(planeShape)
+  planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+  world.addBody(planeBody)
+
+  function animateMap() {
+    requestAnimationFrame(animateMap)
+  
+    //Animation Physics For demon Model
+      planeMesh.position.set(
+      planeBody.position.x,
+      planeBody.position.y,
+      planeBody.position.z
+  )}
+
+  renderer.setAnimationLoop(animateMap)
+  
+  // END --- Physics -----
+
 })
 
 
 // --END: Models ------------------------------------------------------------------------------
 
+// --BEGIN: Physics World------------------------------------------------------------------------------
 
+// Create World with gravity of the earth 
+// (negative number, so that objects are attracted towards the bottom)
+const world = new CANNON.World()
+world.gravity.set(0, -9.81, 0)
+
+// --END: Physics  World------------------------------------------------------------------------------
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -208,11 +340,17 @@ const tick = () => {
     mixer.update(deltaTime);
   }
 
+  world.fixedStep() // default: 1 / 60 meaning 60fps
+  // characterPhysicsBody.animate()
+  // characterPhysicsBody.setWorldChange(deltaTime)
+
+
   // Update controls
   controls.update()
 
   // Render
-  new OutlineEffect(renderer).render(scene, camera)
+  //new OutlineEffect(renderer).render(scene, camera)
+  renderer.render(scene, camera)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
